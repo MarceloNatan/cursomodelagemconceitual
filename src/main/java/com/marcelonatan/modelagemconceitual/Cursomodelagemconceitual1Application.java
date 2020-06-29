c041878a4b186b3d0a87b43a3d3c5a6fcaf0815e
Marcelo Natan <marcelonvasc@gmail.com>
2020-06-29 19:50:36 -0300

    Projeto criado

---
+++ src/main/java/com/marcelonatan/modelagemconceitual/Cursomodelagemconceitual1Application.java
@@ -0,0 +1,13 @@
+package com.marcelonatan.modelagemconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomodelagemconceitual1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomodelagemconceitual1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelonatan/modelagemconceitual/Cursomodelagemconceitual1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelonatan.modelagemconceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomodelagemconceitual1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
