96c0d65fec0df4d840c1c0bab8bf5d59560f0f74
Marcelo Natan <marcelonvasc@gmail.com>
2020-06-30 09:47:56 -0300

    Criando operacao de instaciacao

--- src/main/java/com/marcelonatan/modelagemconceitual/Cursomodelagemconceitual1Application.java
+++ src/main/java/com/marcelonatan/modelagemconceitual/Cursomodelagemconceitual1Application.java
@@ -1,13 +1,32 @@
 package com.marcelonatan.modelagemconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomodelagemconceitual1Application {
+import com.marcelonatan.modelagemconceitual.domain.Categoria;
+import com.marcelonatan.modelagemconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomodelagemconceitual1Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomodelagemconceitual1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
